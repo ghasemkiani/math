b9c161d1d6d0b28e4b8f911f787fd84859e676e0
Ghasem Kiani <ghasemkiani@gmail.com>
2019-10-06 18:32:03 +0330

    add MUtil.exp

--- mutil.js
+++ mutil.js
@@ -19,6 +19,9 @@ class MUtil extends Base {
 	log(n, b) {
 		return cutil.isNil(b) ? Math.log(n) : Math.log(n) / Math.log(b);
 	}
+	exp(n, b) {
+		return cutil.isNil(b) ? Math.exp(n) : b ** n;
+	}
 }
 
 const mutil = new MUtil();
