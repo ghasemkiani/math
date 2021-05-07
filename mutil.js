74ad2d5ad036542793df1f59539f7fd3187bc574
Ghasem Kiani <ghasemkiani@gmail.com>
2021-05-07 08:21:44 +0430

    update

--- mutil.js
+++ mutil.js
@@ -1,7 +1,7 @@
 //	@ghasemkiani/math/mutil
 
-const {cutil} = require("@ghasemkiani/commonbase/cutil");
-const {Base} = require("@ghasemkiani/commonbase/base");
+const {cutil} = require("@ghasemkiani/base/cutil");
+const {Obj: Base} = require("@ghasemkiani/base/obj");
 
 class MUtil extends Base {
 	geometricProgressionSum(r, n) {
